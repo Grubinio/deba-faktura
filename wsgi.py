e04400e44869c0861b429ac5b01ad5e9212a27fb
Grubinio <georggruben@gmail.com>
2025-04-01 21:10:36 +0200

    Umgebungsvariablen debug

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,13 @@ import sys
 from dotenv import load_dotenv
 import os
 
+# .env manuell laden – wichtig für Apache/mod_wsgi!
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Projektpfad eintragen
+sys.path.insert(0, os.path.dirname(__file__))
+
 ##Für Umgebungsvariablen
 # Hier wird der Pfad zur .env-Datei gesetzt
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
