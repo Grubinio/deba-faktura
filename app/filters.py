db6167df346a290606f56a6408a7e586d3b48747
Grubinio <georggruben@gmail.com>
2025-04-08 15:06:28 +0200

    Tabelle auf Home Seite

--- app/filters.py
+++ app/filters.py
@@ -3,3 +3,10 @@ def format_currency(value):
         return f"{value:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") + " €"
     except (TypeError, ValueError):
         return "-"
+    
+@app.template_filter('datum_de')
+def format_datum(value):
+    try:
+        return value.strftime('%d.%m.%Y')
+    except:
+        return "–"
